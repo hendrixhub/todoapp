428596523ad00ef7df13f35d998cdda0b65a3e96
John Hendrix <jthendrix@mac.com>
2018-03-29 19:16:45 -0400

    Push project to GitHub

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+// src/config/index.js
+
+module.exports = {
+    appName: 'Things To Do',
+    port: 3030,
+    db: {
+      username: 'webuser',
+      password: 'webuser_pass',
+      host: 'ds147228.mlab.com:47228',
+      dbName: 'jdbase',
+    }
+  
+  }
